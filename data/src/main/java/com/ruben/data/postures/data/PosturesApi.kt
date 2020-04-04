437d1d1c2d0a929a19befc27ad95e67d0d711832
Rubén Viciana <ruben@safeboda.com>
2020-04-04 16:17:22 +0200

    [YS-1] [BOY-SCOUT] Change Path to Query in wenservice call

--- data/src/main/java/com/ruben/data/postures/data/PosturesApi.kt
+++ data/src/main/java/com/ruben/data/postures/data/PosturesApi.kt
@@ -3,18 +3,17 @@ package com.ruben.data.postures.data
 import com.ruben.data.postures.entity.PostureDetailResponse
 import com.ruben.data.postures.entity.PostureResponse
 import io.reactivex.Single
-import retrofit2.adapter.rxjava2.Result
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PosturesApi {
 
     @GET("postures")
     fun postures(): Single<List<PostureResponse>>
 
-    @GET("posture_detail/{id}")
+    @GET("posture_detail")
     fun postureDetail(
-        @Path("id") id: String
+        @Query("_id") id: String
     ): Single<PostureDetailResponse>
 
 }
